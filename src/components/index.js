f9c3c9fa28bfe9f84d61026ce8116f9a5783369c
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-18 21:17:50 +0100

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import HeroImg from "./HeroImg";
+import Stats from "./Stats";
+import Earn from "./Earn";
+import CardDeal from "./CardDeal";
+import RarityLevel from "./RarityLevel";
+import RarityTable from "./RarityTable";
+import Team from "./Team";
+import Faqs from "./Faqs";
+import Footer from "./Footer";
+
+export {
+  Navbar,
+  Hero,
+  HeroImg,
+  Stats,
+  Earn,
+  CardDeal,
+  RarityLevel,
+  RarityTable,
+  Team,
+  Faqs,
+  Footer,
+};
