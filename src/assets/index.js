2b18b643a383bc778d039b79b34c9eb76e6545a5
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-19 15:41:44 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,35 @@
 import Logo from "./Logo.svg";
+import linkedin from "./linkedin.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import twitter from "./twitter.svg";
+import feet from "./feet.svg";
+import currency from "./currency.svg";
+import timer from "./timer.svg";
+import stats1 from "./stats1.svg";
+import stats2 from "./stats2.svg";
+import stats3 from "./stats3.svg";
+import stats4 from "./stats4.svg";
+import road from "./road.svg";
+import judge from "./judge.svg";
+import people from "./people.svg";
+import fire from "./fire.svg";
 
-export { Logo };
+export {
+  Logo,
+  linkedin,
+  facebook,
+  instagram,
+  twitter,
+  feet,
+  currency,
+  timer,
+  stats1,
+  fire,
+  stats2,
+  road,
+  stats3,
+  judge,
+  stats4,
+  people,
+};
