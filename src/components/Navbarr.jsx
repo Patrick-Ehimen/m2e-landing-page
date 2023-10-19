82ad85d251324e76c98f1ad9baded88171258989
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-19 19:29:48 +0100

    Update Navbarr.jsx

--- src/components/Navbarr.jsx
+++ src/components/Navbarr.jsx
@@ -11,45 +11,18 @@ const Navbarr = () => {
         <div className="flex md:order-2">
           <button
             type="button"
-            className="text-primary bg-[#D9D9D9C9] hover:bg-[#d9d9d9df] focus:ring-4 focus:outline-none  font-bold font-inter rounded-full text-sm px-4 py-2 text-center mr-3 md:mr-0 "
+            className="text-primary bg-[#D9D9D9C9] hover:bg-[#d9d9d9df]   font-bold font-inter rounded-full text-sm px-4 py-2 text-center mr-3 sm:flex hidden"
           >
             Launch App
           </button>
-          <button
-            data-collapse-toggle="navbar-sticky"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
         </div>
-        <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-sticky"
-        >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div className="items-center justify-between hidden w-full md:flex md:w-auto">
+          <ul className="flex flex-col mt-3 font-medium  text-[15px] md:flex-row md:space-x-8 text-white font-inter text-lg my-2 ">
             {navLinks.map((link) => (
               <li key={link.id}>
                 <a
                   href={`#${link.id}`}
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 text-white hover:bg-[#d9d9d9df] md:hover:bg-transparent  md:p-0   "
                 >
                   {link.title}
                 </a>
