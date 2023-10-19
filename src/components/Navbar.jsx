e33d3097c95a00173deb63e563571963edbbce16
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-19 10:12:47 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
+import { Logo } from "../assets";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  // return <nav className="fixed bg-white  w-[5/6] h-[50px] ">
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-transparent border border-gray-200 rounded-full shadow-lg px-2 py-3 mx-10 mt-10">
+      <div className="container w-[36px] h-[50px]"></div>
+      <img src={Logo} alt="logo" />
+    </nav>
+  );
 };
 
 export default Navbar;
