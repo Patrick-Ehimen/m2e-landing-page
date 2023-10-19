e0069ff024f5aef47b88a916af83bfee7953e2ae
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-19 20:17:39 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,83 @@
-import { Logo } from "../assets";
+import { useState } from "react";
+
+import { Logo, menu, close } from "../assets";
 import { navLinks } from "../constants";
 
-const Navbar = () => {
-  // return <nav className="fixed bg-white  w-[5/6] h-[50px] ">
-  return (
-    <nav className="fixed top-0 left-0 right-0 z-10  bg-[#ffffff0a] bg-[#000000] border border-black rounded-full shadow-lg px-2 py-3 mx-20 mt-10">
-      <div className="container w-[36px] h-[35px]">
-        <img src={Logo} alt="logo" className="w-[36px] h-[30px] ml-6 my-2" />
-      </div>
+// Navbar component
+// Renders the navigation bar
+const Navbarr = () => {
+  // Using React hook for handling toggle state
+  const [toggle, settoggle] = useState(false);
 
-      <ul></ul>
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10  bg-[#ffffff0a] bg-[#000000] border border-black rounded-full shadow-lg px-2 py-3 mx-20 mt-10 ">
+      {/* Navigation bar content */}
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
+        {/* Logo */}
+        <a href="#" className="flex items-center">
+          <img src={Logo} alt="logo" className="w-[36px] h-[30px] ml-6 my-2" />
+        </a>
+        {/* Launch App button */}
+        <div className="flex md:order-2">
+          <button
+            type="button"
+            className="text-primary bg-[#D9D9D9C9] hover:bg-[#d9d9d9df]   font-bold font-inter rounded-full text-sm px-4 py-2 text-center mr-3 sm:flex hidden"
+          >
+            Launch App
+          </button>
+        </div>
+        {/* Navigation links */}
+        <div className="items-center justify-between hidden w-full md:flex md:w-auto">
+          <ul className="flex flex-col mt-3 font-medium  text-[15px] md:flex-row md:space-x-8 text-white font-inter text-lg my-2 ">
+            {navLinks.map((link) => (
+              <li key={link.id}>
+                <a
+                  href={`#${link.id}`}
+                  className="block py-2 pl-3 pr-4 text-white hover:bg-[#d9d9d9df] md:hover:bg-transparent  md:p-0   "
+                >
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          {/* Menu button which toggles visibility of navbar */}
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain"
+            onClick={() => settoggle((prev) => !prev)}
+          />
+          {/* Sidebar menu */}
+          <div
+            className={`${
+              toggle ? "flex" : "hidden"
+            } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          >
+            <ul className="list-none flex flex-col justify-end items-center flex-1">
+              {navLinks.map((nav, index) => (
+                // Each item in sidebar menu
+                <li
+                  key={nav.id}
+                  className={`font-inter font-normal cursor-pointer text-[16px] ${
+                    index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                  } text-white mr-10`}
+                >
+                  <a
+                    href={`#${nav.id}`}
+                    className="text-white hover:text-secondary"
+                  >
+                    {nav.title}
+                  </a>{" "}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
 
-export default Navbar;
+export default Navbarr;
