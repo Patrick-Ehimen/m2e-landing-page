784156624a22537eb5be28448d86d62cbcfd2fff
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-19 10:12:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,6 @@ import {
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      {/* <div className="fixed w-1480px h-100px top-0 left-0 bg-[#ffffff0f] rounded-[50px] backdrop-blur-[20px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(20px)_brightness(100%)]"> */}
-
-      {/* </div> */}
       <Navbar />
       <Hero />
       <HeroImg />
