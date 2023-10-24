feab0707ca2678d8e4a9f90cbd5d2e72e4719e97
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-24 14:13:43 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,22 @@
+import { Swift } from "../assets";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="flex justify-center items-start">
+      <img
+        className="flex md:mt-[250px] mt-[200px] md:w-1/2 w-3/6"
+        alt="Swift blockchain"
+        src={Swift}
+      />
+      <div>
+        <p>
+          Ready to energize your life and turn it into earnings? With Move to
+          Earn, your steps hold real value. Simply walk, run, or exercise to
+          earn. Each step matters.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
