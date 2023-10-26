f06e8d9b7f25ed571469cecc2a67cfe9b13dc905
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-26 20:52:10 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,21 +18,20 @@ const Hero = () => {
         </p>
       </div>
 
-      <div className="flex flex-row flew-warp sm:mt-10 mt-6">
+      <div className="flex flex-row flew-warp sm:mt-10 mt-6 ">
         <img
           src={Apple3}
           alt="apple"
-          className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          className="w-[128px] h-[42px] object-contain mr-5 hover:cursor-pointer"
         />
         <img
           src={google2}
           alt="google-play"
-          className="w-[128px] h-[42px] object-contain cursor-pointer"
+          className="w-[128px] h-[42px] object-contain hover:cursor-pointer"
         />
 
         {/* gradient start */}
-        <div className="fixed  w-[50%] h-[50%] top-0 left-0 navbar__gradient" />
-        <div className="absolute  w-[40%] h-[35%] hero__gradient" />
+        <div className="absolute z-[1] w-[50%] h-[50%] top-0 left-0 navbar__gradient" />
         {/* gradient end */}
       </div>
     </section>
