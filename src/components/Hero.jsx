e707d53daf6053c049718ef9713cb23012ebcdbe
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-26 13:15:56 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,41 @@
-import { Swift } from "../assets";
+import { Swift, Apple3, google2 } from "../assets";
 
 const Hero = () => {
   return (
-    <div className="flex justify-center items-start">
+    <section className="flex flex-col items-center">
       <img
-        className="flex md:mt-[250px] mt-[200px] md:w-1/2 w-3/6"
+        className=" sm:mt-[250px] mt-[120px] px-10 sm:px-10 sm:max-w-3xl max-w-sm"
         alt="Swift blockchain"
         src={Swift}
       />
       <div>
-        <p>
+        <p className="flex items-center mx-[50px] sm:mx-[120px] mt-8 text-white text-center font-inter text-lg">
           Ready to energize your life and turn it into earnings? With Move to
+          <br className="md:block hidden" />
           Earn, your steps hold real value. Simply walk, run, or exercise to
-          earn. Each step matters.
+          <br className="md:block hidden " />
+          Each step matters.
         </p>
       </div>
-    </div>
+
+      <div className="flex flex-row flew-warp sm:mt-10 mt-6">
+        <img
+          src={Apple3}
+          alt="apple"
+          className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+        />
+        <img
+          src={google2}
+          alt="google-play"
+          className="w-[128px] h-[42px] object-contain cursor-pointer"
+        />
+
+        {/* gradient start */}
+        <div className="fixed  w-[50%] h-[50%] top-0 left-0 navbar__gradient" />
+        <div className="absolute  w-[40%] h-[35%] hero__gradient" />
+        {/* gradient end */}
+      </div>
+    </section>
   );
 };
 
