b44d5bce97c48455a3508670cab44beff92565bd
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-27 11:17:49 +0100

    Update Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,20 +2,30 @@ import { stats } from "../constants";
 
 const Stats = () => {
   return (
-    <section className="flex flex-wrap justify-between w-1/2 mt-10 mx-[250px] p-5">
+    <section className="flex flex-wrap justify-between w-1/2 mt-10 mx-auto p-5">
       {stats.map((stat) => (
         <div
           key={stat.id}
-          className="w-48 h-48 relative mb-10 pr-10 bg-transparent-white rounded-2xl justify-center items-center"
+          className="w-48 h-48 relative mb-10 px-5 bg-transparent-white rounded-2xl justify-center cursor-pointer items-center"
         >
-          <div className="absolute top-2 right-2 rounded-full overflow-hidden">
+          <div className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 rounded-full">
             <img src={stat.img} alt={stat.name} className="w-12 h-12" />
           </div>
-          <div className="p-5">
-            <img src={stat.imgContent} alt={stat.name} className="mb-2 w-2/3" />
-            <p className="mb-2 text-white">{stat.name}</p>
-            <h2 className="font-bold text-white">{stat.content}</h2>
+
+          <div className="p-5 pl-2">
+            <img
+              src={stat.imgContent}
+              alt={stat.name}
+              className="mb-2 w-12/12"
+            />
+            <p className="mb-2 text-neutral-600 text-sm font-bold font-Inter">
+              {stat.name}
+            </p>
+            <h2 className="font-bold font-Inter text-white mt-12">
+              {stat.content}
+            </h2>
           </div>
+          <div className="sm:hidden absolute z-0 w-[40%] h-[35%] bottom-0 hero__gradient" />
         </div>
       ))}
     </section>
