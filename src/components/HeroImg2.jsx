3c11a9dbf2d85285a2e1b6cd4d6dab09226466ff
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-27 01:57:03 +0100

    Create HeroImg2.jsx

---
+++ src/components/HeroImg2.jsx
@@ -0,0 +1,42 @@
+import { clients } from "../constants";
+
+const HeroImg = () => {
+  return (
+    <section className="flex flex-col items-center pt-16 pb-8">
+      <div className="container flex flex-col items-center">
+        <div className="flex flex-wrap justify-center hover:scale z-10">
+          {clients.slice(0, 6).map((client) => (
+            <div
+              key={client.id}
+              className="flex justify-center items-center w-20 h-20 bg-white bg-opacity-10 rounded-[34px] backdrop-blur-2xl m-1 sm:m-2 hover:scale-105 cursor-pointer"
+            >
+              <img
+                src={client.logo}
+                alt="client_logo"
+                className="object-contain w-1/2"
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="flex flex-wrap justify-center w-full max-w-4xl z-10">
+          {clients.slice(6).map((client) => (
+            <div
+              key={client.id}
+              className="flex justify-center items-center w-20 h-20 bg-white bg-opacity-10 rounded-[34px] backdrop-blur-2xl m-1 sm:m-2 hover:scale-105 cursor-pointer"
+            >
+              <img
+                src={client.logo}
+                alt="client_logo"
+                className="object-contain w-1/2"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="absolute z-0 w-[40%] h-[35%] bottom hero2__gradient" />
+      </div>
+    </section>
+  );
+};
+
+export default HeroImg;
