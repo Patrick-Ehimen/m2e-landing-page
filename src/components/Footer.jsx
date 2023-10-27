2978cfeb6e9c7cd1e7f0ee024feeb636159e28c8
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-27 22:18:35 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,72 @@
-const Footer = () => {
-  return <div>Footer</div>;
-};
+import styles from "../styles";
+import { Logo } from "../assets";
+import { footerLinks, socialMedia } from "../constants";
+
+const Footer = () => (
+  <section className="flex justify-center items-center mt-10 mx-[50px] sm:mx-[200px] sm:py-16 py-6 flex-col">
+    <div className="flex justify-center items-start md:flex-row flex-col mb-8 w-full">
+      <div className="flex-1 flex flex-col justify-start mr-10">
+        <img
+          src={Logo}
+          alt="hoobank"
+          className="w-[266px] h-[72px] object-contain"
+        />
+        <p className={`${styles.paragraph} mt-4 max-w-[310px] text-slate-500`}>
+          Ready to energize your life and turn it into earnings? With Move to
+          Earn, your steps hold real value. Simply walk, run, or exercise to
+          Each step matters.
+        </p>
+      </div>
+
+      <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10 text-slate-500">
+        {footerLinks.map((footerLink) => (
+          <div
+            key={footerLink.title}
+            className="flex flex-col ss:my-0 my-4 min-w-[150px]"
+          >
+            <h4 className="font-Inter font-bold text-[20px] leading-[27px] text-white">
+              {footerLink.title}
+            </h4>
+            <ul className="list-none mt-4">
+              {footerLink.links.map((link, index) => (
+                <li
+                  key={link.name}
+                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
+                    index !== footerLink.links.length - 1
+                      ? "mb-4"
+                      : "mb- undefined"
+                  }`}
+                >
+                  {link.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+        © 2023 Swift Earn. All Rights Reserved.
+      </p>
+
+      <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((social, index) => (
+          <img
+            key={social.id}
+            src={social.icon}
+            alt={social.id}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+            }`}
+          />
+        ))}
+      </div>
+
+      <div className="absolute z-[0] w-[50%] h-[15%] left footer_gradient " />
+    </div>
+  </section>
+);
 
 export default Footer;
