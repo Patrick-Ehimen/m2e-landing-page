4ff24ef0c90af147e3fb003ddf1295846f06d942
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-27 02:22:31 +0100

    Update RarityLevel.jsx

--- src/components/RarityLevel.jsx
+++ src/components/RarityLevel.jsx
@@ -1,5 +1,17 @@
+import { Rarity } from "../assets";
+
 const RarityLevel = () => {
-  return <div>RarityLevel</div>;
+  return (
+    <section className="flex flex-col justify-center items-center">
+      <h2 className="font-Inter font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] mt-10 sm:mt-5">
+        Rarity Level
+      </h2>
+      <p className="font-Inter font-normal text-white text-[14px] leading-[30.8px] mx-2 mt-5">
+        More infomation about the rarity levels of NFTs
+      </p>
+      <img src={Rarity} alt="rarity" className="sm:w-3/12 w-1/2" />
+    </section>
+  );
 };
 
 export default RarityLevel;
