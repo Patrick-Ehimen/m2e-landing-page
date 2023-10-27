05f5f88bec454256883e5c5f548bbec3ac29db6a
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-27 15:14:56 +0100

    Update RarityTable.jsx

--- src/components/RarityTable.jsx
+++ src/components/RarityTable.jsx
@@ -1,5 +1,16 @@
+import { rarityTable } from "../assets";
+
 const RarityTable = () => {
-  return <div>RarityTable</div>;
+  return (
+    <section className="flex flex-col justify-center items-center">
+      <img
+        src={rarityTable}
+        alt="rarity"
+        className="sm:w-7/12 w-9/12 mt-5 z-[10] bg-[rgba(0, 0, 0, 0.6)] mb-5"
+      />
+      <div className="absolute z-[0] w-[50%] h-[15%] left card2_gradient " />
+    </section>
+  );
 };
 
 export default RarityTable;
