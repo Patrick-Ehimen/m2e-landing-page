b4213b8158660c3d8165959d3bfcc13ee9ec5b4e
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-27 15:14:50 +0100

    Update Earn.jsx

--- src/components/Earn.jsx
+++ src/components/Earn.jsx
@@ -1,5 +1,17 @@
+import { Steps } from "../assets";
+
 const Earn = () => {
-  return <div>Earn</div>;
+  return (
+    <section className="flex flex-col justify-center items-center">
+      <img
+        src={Steps}
+        alt="rarity"
+        className="sm:w-6/12 w-8/12 z-10 my-10 sm:my-20"
+      />
+
+      <div className="absolute z-0 w-[40%] h-[35%] earn__gradient" />
+    </section>
+  );
 };
 
 export default Earn;
